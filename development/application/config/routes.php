36bf99af2f8fc2fbabfce1ee9a5e8f2227bda92b
Jovic Abengona <jvlabengona@gmail.com>
2023-02-24 16:13:23 +0800

    Fixed .htaccess; Added login page and assets

--- development/application/config/routes.php
+++ development/application/config/routes.php
@@ -49,6 +49,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+
+/* User Routes */
+$route['default_controller'] = 'users';
+
+/* Documentation Routes */
+$route['admin_documentations'] = 'documentations/admin_documentations';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
