13b14c9ea77fc06c5e6c7a25f4b9cb3d7486d3c2
Jovic Abengona <jvlabengona@gmail.com>
2023-03-01 14:41:19 +0800

    Refactor delete documentation feature

--- development/application/controllers/Documentations.php
+++ development/application/controllers/Documentations.php
@@ -140,57 +140,17 @@ class Documentations extends CI_Controller {
 
 		try {
 			if($_SESSION["user_level_id"] == USER_LEVEL["ADMIN"]){
-				$this->db->trans_start();
-				$delete_documentation = $this->Documentation->deleteDocumentation($_POST["remove_documentation_id"]);
-
-				if($delete_documentation["status"]){
-					/* Remove remove_documentation_id in documentations_order and update documentations_order in workpsaces table */
-					$documentations_order = $this->Workspace->getDocumentationsOrder($_SESSION["workspace_id"]);
-
-					if($documentations_order["status"]){
-						$documentations_order = explode(",", $documentations_order["result"]["documentation_ids_order"]);
-						
-						$documentation_index = array_search($_POST["remove_documentation_id"], $documentations_order);
-						
-						if($documentation_index !== FALSE){
-							unset($documentations_order[$documentation_index]);
-							$documentations_count = count($documentations_order);
-	
-							$documentations_order = ($documentations_count) ? implode(",", $documentations_order) : "";
-							$update_workpsace = $this->Workspace->updateDocumentationsIdsOrder(array("documentations_order" => $documentations_order, "workspace_id" => $_SESSION["workspace_id"]));
-
-							if($update_workpsace["status"]){
-								if(($_POST["remove_is_archived"] == FALSE_VALUE && !$documentations_count) || ($_POST["remove_is_archived"] == TRUE_VALUE && $_POST["archived_documentations"] == "0")){
-									$message = ($_POST["remove_is_archived"] == FALSE_VALUE) ? "You have no documentations yet." : "You have no archived documentations yet.";
-			
-									$response_data["result"]["is_archived"]            = $_POST["remove_is_archived"];
-									$response_data["result"]["no_documentations_html"] = $this->load->view('partials/no_documentations_partial.php', array('message' => $message, true));
-								}
-							}
-							else{
-								throw new Exception($update_workpsace["error"]);
-							}
-						}
-	
-						$response_data["status"] = true;
-						$response_data["result"]["documentation_id"] = $_POST["remove_documentation_id"];
-						// $this->db->trans_complete();
-					}
-					else{
-						throw new Exception($documentations_order["error"]);
-					}
-				}
-				else{
-					throw new Exception($delete_documentation["error"]);
-				}
+				$response_data = $this->Documentation->deleteDocumentation($_POST);
 			}
 			else {
 				throw new Exception("You are not allowed to do this action!");
 			}
 		}
 		catch (Exception $e) {
-			$this->db->trans_rollback();
+			$response_data["error"] = $e->getMessage();
 		}
+
+		echo json_encode($response_data);
 	}
 
 	// Private functions
