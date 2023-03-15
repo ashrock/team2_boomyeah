e3e47804d74bdd4d27af0c19afc4811aca2cea1a
Jovic Abengona <jvlabengona@gmail.com>
2023-03-15 13:06:29 +0800

    Merge v0.3 to BE develop

--- development/application/views/partials/confirm_action_modals.php
+++ development/application/views/partials/confirm_action_modals.php
@@ -6,7 +6,9 @@
         </div>
         <div class="modal-footer">
             <form action="<?= BASE_FILE_URL ?>processes/manage_documentation.php" method="POST" id="remove_comment_form">
+                <input type="hidden" name="action" value="remove_comment" class="action">
                 <input type="hidden" name="comment_id" value="" class="comment_id">
+                <input type="hidden" name="post_id" value="" class="post_id">
                 <button type="button" class="modal-close waves-effect btn-flat no_btn">No</button>
                 <button type="submit" class="modal-close waves-effect btn-flat yes_btn">Yes</button>
             </form>
