2954a98f928ecd95bef3395b3d718965ae6e7728
Jovic Abengona <jvlabengona@gmail.com>
2023-03-17 11:55:08 +0800

    Merge v0.3 to BE develop

--- development/assets/js/custom/module_tab_comments/module_tab_comments_be.js
+++ development/assets/js/custom/module_tab_comments/module_tab_comments_be.js
@@ -122,7 +122,7 @@ function onAddPostComment(event){
             let comments_list = ux(comment_id).find(".replies_list");
             let toggle_replies_btn = ux(comment_id).find(".toggle_replies_btn");
 
-            if(toggle_replies_btn.self()){
+            if(!toggle_replies_btn.self().classList.contains("hidden")){
                 toggle_replies_btn.find("b").trigger("click");
             } else {
                 comments_list.append(response_data.result.html);
@@ -150,7 +150,8 @@ function showRepliesList(event){
         let post_comments_form = ux("#fetch_post_comments_form");
         post_comments_form.find(".post_id").val(post_id);
         post_comments_form.trigger("submit");
-        ux(show_replies_btn).remove();
+        
+        ux(show_replies_btn).addClass("hidden");
     }
 }
 
@@ -232,14 +233,19 @@ function onCommentMessageKeypress(event){
 
     if(event.which === KEYS.ENTER){
         event.preventDefault();
+
         let submit_form = (post_form) ? post_form : edit_comment_form;
-        
         ux(submit_form).trigger("submit");
     }
     
-    if(edit_comment_form && event.which === KEYS.ESCAPE){
+    if(event.which === KEYS.ESCAPE){
         /** Close edit form */
-        closeEditCommentForm(event);
+        if(edit_comment_form){
+            closeEditCommentForm(event);
+        } else {
+            post_form.reset();
+            ux(post_form).removeClass("show");
+        }
     }
 }
 
