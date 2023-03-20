46e7af7a5839904e91aa3bd18ad424d964b9e318
Jan Dreau Ganggangan <v88jadee@Jans-MacBook-Air.local>
2023-03-20 09:26:03 +0800

    Initial commit for v04

--- clickable_prototype/v0.4/assets/js/custom/module_tab_comments/module_tab_comments_be.js
+++ clickable_prototype/v0.4/assets/js/custom/module_tab_comments/module_tab_comments_be.js
@@ -29,6 +29,8 @@ document.addEventListener("DOMContentLoaded", async (event) => {
         .on("keydown", ".edit_comment_form .comment_message", onEditMessageKeypress)
         .on("click", ".edit_comment_form .cancel_btn", closeEditCommentForm)
         ;
+
+    ux(".fetch_tab_posts_btn").trigger("click");
 });
 
 function onSubmitPostForm(event){
