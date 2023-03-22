ff36e50f87ce6a59912104f685e805a2ed701bb6
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 <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>
2023-03-22 21:16:50 +0800

    updated internal documentation for v04

--- clickable_prototype/v0.4/index.php
+++ clickable_prototype/v0.4/index.php
@@ -11,28 +11,27 @@
         <meta name="author" content="UX Team 2">
         <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/x-icon">
         <meta name="description" content="A great way to describe your documentation tool">
-        <title>Boom Yeah | Team 2 | Clickable Prototype V0.3</title>
+        <title>Boom Yeah | Team 2 | Clickable Prototype V0.4</title>
         <link rel="stylesheet" href="./assets/css/index.css"/>
     </head>
     <body>
         <div id="wrapper">
             <div class="context_container v03">
-                <h1>Clickable Prototype v.03 Links</h1>
+                <h1>Clickable Prototype v.04 Links</h1>
                 <a href="./views/pages/admin_edit_section.php">Admin Edit Section Page</a>
                 <p>
                     <span> User Stories</span>
-                    P9 As an admin, I want to create/manage modules and tabs to the documentations so that I can impart and guide people on different areas. <br/><br/>
-                    P7 As an admin, I want to have a preview of the documentation so that I won't feel stressed trying to remember what's in it and prevent unnecessary repeated asking the same question.
+                    (P6)As an admin, I want to upload files/images so that I can easily use them in the documentations without much effort.
                 </p>
                 <a href="./views/pages/user_view_section.php">User View Section Page</a>
                 <p>
                     <span>User Stories</span>
-                    (P7) As a user, I want to be able to watch videos, see images, discuss, and read text in a documentation so that I can see the meaning and feel the importance of being in a team.<br/><br/>
+                    (P7) As a user, I want to be able to watch videos, see images, discuss, and read text in a documentation so that I can see the meaning and feel the importance of being in a team.</br></br>
                     (P6) As a user, I want to be able to update and delete my comment so that I can be more confident that my thoughts and feelings will be seen by the rest of the community whichever way I like.
                 </p>
                 <h2>Figma Design</h2>
-                <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" class="frame_display" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F7sD1iBde0wOo0CjXn01mqU%2FBoomYEAH%3Fnode-id%3D2614%253A125173%26t%3Dr1CCtQ4mbfEUrEgb-1" allowfullscreen></iframe>
-                <a href="https://tinyurl.com/Roadmap-Spreadsheet-Link-v02" target="_blank">Product Roadmap Spreadsheet Link</a>
+                <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" class="frame_display" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F7sD1iBde0wOo0CjXn01mqU%2FBoomYEAH%3Fnode-id%3D3253%253A125131%26t%3D6ubSpCf4u39EYiPu-1" allowfullscreen></iframe>
+                <a href="https://tinyurl.com/ProductRoadmapSpreadsheetv04" target="_blank">Product Roadmap Spreadsheet Link</a>
             </div>
         </div>
     </body>
