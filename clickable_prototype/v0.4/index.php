28a9a6b21d3b5339b09e0b2d9e6fe0356b7e97fc
Jan Dreau Ganggangan <v88jadee@Jans-MacBook-Air.local>
2023-03-23 07:32:14 +0800

    Removed User side user stories

--- clickable_prototype/v0.4/index.php
+++ clickable_prototype/v0.4/index.php
@@ -23,12 +23,6 @@
                     <span> User Stories</span>
                     (P6)As an admin, I want to upload files/images so that I can easily use them in the documentations without much effort.
                 </p>
-                <a href="./views/pages/user_view_section.php">User View Section Page</a>
-                <p>
-                    <span>User Stories</span>
-                    (P7) As a user, I want to be able to watch videos, see images, discuss, and read text in a documentation so that I can see the meaning and feel the importance of being in a team.</br></br>
-                    (P6) As a user, I want to be able to update and delete my comment so that I can be more confident that my thoughts and feelings will be seen by the rest of the community whichever way I like.
-                </p>
                 <h2>Figma Design</h2>
                 <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" class="frame_display" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F7sD1iBde0wOo0CjXn01mqU%2FBoomYEAH%3Fnode-id%3D3253%253A125131%26t%3D6ubSpCf4u39EYiPu-1" allowfullscreen></iframe>
                 <a href="https://tinyurl.com/ProductRoadmapSpreadsheetv04" target="_blank">Product Roadmap Spreadsheet Link</a>
